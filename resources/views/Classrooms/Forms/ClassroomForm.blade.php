439acd735d272c036e6dd8e0507c459fcf5b3348
thalisson <thalissonsv22@gmail.com>
2022-07-14 22:27:59 -0300

    adiciona CRUD para materias.

--- resources/views/Classrooms/Forms/ClassroomForm.blade.php
+++ resources/views/Classrooms/Forms/ClassroomForm.blade.php
@@ -1,56 +1,95 @@
 {{-- Page Content --}}
 @section('content')
-  <form action="#" method="POST" class="p-3 my-2 mx-auto form-add-edit">
+    @if ($route_form)
+        <form action="{{ route('store.classroom') }}" method="POST" class="p-3 my-2 mx-auto form-add-edit">
+    @else
+        <form action="{{ route('update.classroom', ['id_classroom' => $id ]) }}" method="POST" class="p-3 my-2 mx-auto form-add-edit">
+    @endif
+    @csrf
     <div class="col-md-12 ps-0">
-      <label class="form-label">
+    <label class="form-label">
         Série
-        <select class="form-select">
-          <option selected>Selecione a série</option>
-          <option value="9">9º</option>
-          <option value="8">8º</option>
-          <option value="7">7º</option>
-          <option value="6">6º</option>
+        <select class="form-select" name="serie">
+            @for ($i = 0; $i <= 9; $i++)
+                @if ($route_form)
+                    @if ($i == 0)
+                        <option value="" selected>Selecione a série</option>
+                    @else
+                        <option value="{{ $i }}">{{ $i }}º</option>
+                    @endif
+                @elseif ($i !== 0)
+                    @if ($i == $room['serie'])
+                        <option value="{{ $i }}" selected>{{ $i }}º</option>
+                    @else
+                        <option value="{{ $i }}">{{ $i }}º</option>
+                    @endif
+                @endif
+            @endfor
         </select>
-      </label>
+    </label>
     </div>
 
     <div class="ps-0">
-      <label class="form-label">
+    <label class="form-label">
         Sigla
-        <select class="form-select">
-          <option selected>Selecione a sigla</option>
-          <option value="A">A</option>
-          <option value="B">B</option>
+        <select class="form-select" name="letter">
+            @foreach (['Selecione a sigla', 'A', 'B', 'C', 'D'] as $key => $value)
+                @if ($route_form)
+                    @if ($key == 0)
+                        <option value="" selected>{{ $value }}</option>
+                    @else
+                        <option value="{{ $value }}">{{ $value }}</option>
+                    @endif
+                @elseif ($key !== 0)
+                    @if ($value == $room['letter'])
+                        <option value="{{ $value }}" selected>{{ $value }}</option>
+                    @else
+                        <option value="{{ $value }}">{{ $value }}</option>
+                    @endif
+                @endif
+            @endforeach
         </select>
-      </label>
+    </label>
     </div>
 
     <div class="ps-0">
-      <label class="form-label">
+    <label class="form-label">
         Período
-        <select class="form-select">
-          <option selected>Selecione o período</option>
-          <option value="manha">Manhã</option>
-          <option value="tarde">Tarde</option>
+        <select class="form-select" name="period">
+            @foreach (['Selecione o período', 'Manhã', 'Tarde', 'Noite'] as $key => $value)
+                @if ($route_form)
+                    @if ($key == 0)
+                        <option value="" selected>{{ $value }}</option>
+                    @else
+                        <option value="{{ $value }}">{{ $value }}</option>
+                    @endif
+                @elseif ($key !== 0)
+                    @if ($value == $room['period'])
+                        <option value="{{ $value }}" selected>{{ $value }}</option>
+                    @else
+                        <option value="{{ $value }}">{{ $value }}</option>
+                    @endif
+                @endif
+            @endforeach
         </select>
-      </label>
+    </label>
     </div>
 
     <div class="py-2">
-      <button type="submit" class="btn btn-custom">@if ($variavel_dados_temporaria) Salvar @else Adicionar @endif</button>
-      <a href="{{ route('list.classroom') }}" class="btn">cancelar</a>
+    <button type="submit" class="btn btn-custom">Salvar</button>
+    <a href="{{ route('list.classroom') }}" class="btn">cancelar</a>
     </div>
-  </form>
+</form>
 @endsection
 
 {{-- Layout Settings  --}}
 @extends('Layout')
 
 @section('title_page')
-  @if ($variavel_dados_temporaria)
-    Editar Sala - 8ºA Tarde
+  @if ($action)
+    {{ $action }}
   @else
-    Adicionar Sala
+    {{ $action }}
   @endif
 @endsection
 
--- resources/views/Classrooms/ListBuilder/ClassroomListBuilder.blade.php
+++ resources/views/Classrooms/ListBuilder/ClassroomListBuilder.blade.php
@@ -1,133 +1,49 @@
 {{-- Page Content --}}
 @section('content')
-  <div class="row content-area">
-    {{-- Filters --}}
-    @include('Classrooms.Filters.ClassroomFilter')
+    <div class="row content-area">
+        {{-- Filters --}}
+        @include('Classrooms.Filters.ClassroomFilter')
 
-    {{-- Rooms listing table --}}
-    <div class="content table-responsive">
-      <table class="table mt-3">
-        <thead>
-          <tr>
-            <th>Sala</th>
-            <th>Período</th>
-            <th>Professor Responsável</th>
-            <th>Aluno Representante</th>
-            <th>Operações</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>8º A</td>
-            <td>Tarde</td>
-            <td>Eduardo Nascimento Rodrigues</td>
-            <td>Thalisson Manso Rodrigues</td>
-            <td>
-              <div class="btn-group">
-                <a href="{{ route('list.info.classroom', ['id_classroom' => 1]) }}" class="btn btn-sm btn-outline-gray">ver mais</a>
+        {{-- Rooms listing table --}}
+        <div class="content table-responsive">
+            <table class="table mt-3">
+                <thead>
+                    <tr>
+                        <th>Sala</th>
+                        <th>Período</th>
+                        <th>Professor Representante</th>
+                        <th>Aluno Representante</th>
+                        <th>Operações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($rooms as $room)
+                        <tr>
+                            <td>{{ $room['serie'] . 'º ' . $room['letter']}}</td>
+                            <td>{{ $room['period'] }}</td>
+                            <td>{{ $room['representative_teacher'] }}</td>
+                            <td>{{ $room['representative_student']}}</td>
+                            <td>
+                            <div class="btn-group">
+                                <a href="{{ route('list.info.classroom', ['id_classroom' => $room['id'] ]) }}" class="btn btn-sm btn-outline-gray">ver mais</a>
 
-                <button type="button" class="btn btn-outline-gray btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"></button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{ route('edit.classroom', ['id_classroom' => 1 ]) }}">Editar</a></li>
-                  <li><a class="dropdown-item" href="#">Remover</a></li>
-                </ul>
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td>8º A</td>
-            <td>Tarde</td>
-            <td>Eduardo Nascimento Rodrigues</td>
-            <td>Thalisson Manso Rodrigues</td>
-            <td>
-              <div class="btn-group">
-                <a href="{{ route('list.info.classroom', ['id_classroom' => 1]) }}" class="btn btn-sm btn-outline-gray">ver mais</a>
-
-                <button type="button" class="btn btn-outline-gray btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"></button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{ route('edit.classroom', ['id_classroom' => 1 ]) }}">Editar</a></li>
-                  <li><a class="dropdown-item" href="#">Remover</a></li>
-                </ul>
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td>8º A</td>
-            <td>Tarde</td>
-            <td>Eduardo Nascimento Rodrigues</td>
-            <td>Thalisson Manso Rodrigues</td>
-            <td>
-              <div class="btn-group">
-                <a href="{{ route('list.info.classroom', ['id_classroom' => 1]) }}" class="btn btn-sm btn-outline-gray">ver mais</a>
-
-                <button type="button" class="btn btn-outline-gray btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"></button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{ route('edit.classroom', ['id_classroom' => 1 ]) }}">Editar</a></li>
-                  <li><a class="dropdown-item" href="#">Remover</a></li>
-                </ul>
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td>8º A</td>
-            <td>Tarde</td>
-            <td>Eduardo Nascimento Rodrigues</td>
-            <td>Thalisson Manso Rodrigues</td>
-            <td>
-              <div class="btn-group">
-                <a href="{{ route('list.info.classroom', ['id_classroom' => 1]) }}" class="btn btn-sm btn-outline-gray">ver mais</a>
-
-                <button type="button" class="btn btn-outline-gray btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"></button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{ route('edit.classroom', ['id_classroom' => 1 ]) }}">Editar</a></li>
-                  <li><a class="dropdown-item" href="#">Remover</a></li>
-                </ul>
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td>8º A</td>
-            <td>Tarde</td>
-            <td>Eduardo Nascimento Rodrigues</td>
-            <td>Thalisson Manso Rodrigues</td>
-            <td>
-              <div class="btn-group">
-                <a href="{{ route('list.info.classroom', ['id_classroom' => 1]) }}" class="btn btn-sm btn-outline-gray">ver mais</a>
-
-                <button type="button" class="btn btn-outline-gray btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"></button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{ route('edit.classroom', ['id_classroom' => 1 ]) }}">Editar</a></li>
-                  <li><a class="dropdown-item" href="#">Remover</a></li>
-                </ul>
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td>8º A</td>
-            <td>Tarde</td>
-            <td>Eduardo Nascimento Rodrigues</td>
-            <td>Thalisson Manso Rodrigues</td>
-            <td>
-              <div class="btn-group">
-                <a href="{{ route('list.info.classroom', ['id_classroom' => 1]) }}" class="btn btn-sm btn-outline-gray">ver mais</a>
-
-                <button type="button" class="btn btn-outline-gray btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"></button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="{{ route('edit.classroom', ['id_classroom' => 1 ]) }}">Editar</a></li>
-                  <li><a class="dropdown-item" href="#">Remover</a></li>
-                </ul>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                                <button type="button" class="btn btn-outline-gray btn-sm dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"></button>
+                                <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="{{ route('edit.classroom', ['id_classroom' => $room['id'] ]) }}">Editar</a></li>
+                                <li><form action="{{ route('destroy.classroom', ['id_classroom' => $room['id'] ]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="dropdown-item" onclick="return confirm('Deletar esta sala ?')">Remover</b>
+                                </form></li>
+                                </ul>
+                            </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-  </div>
 @endsection
 
 {{-- Layout Settings  --}}
